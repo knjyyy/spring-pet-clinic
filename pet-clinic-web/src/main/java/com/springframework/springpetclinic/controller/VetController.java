b96f5bc2ad2429cbb4adfa2a57339483d158eeb2
kenju <rzzlknjtrndd@gmail.com>
2019-07-12 19:29:19 +0800

    Commit to fix unimported packages.

--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/controller/VetController.java
@@ -3,6 +3,8 @@ package com.springframework.springpetclinic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.springframework.springpetclinic.services.VetService;
+
 @RequestMapping("/vets")
 @Controller
 public class VetController {
